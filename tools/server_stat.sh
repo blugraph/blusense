c7e92e75adc00e0e90b44235e981882afeccebb9
Jaya <mjay@i2r.a-star.edu.sg>
2016-03-18 10:11:53 +0800

    Updated script with correct server URL. also changed the keyword to look for in WPA supplicant conf file.

--- tools/server_stat.sh
+++ tools/server_stat.sh
@@ -5,9 +5,12 @@
 
 WPA_DIR="/etc/wpa_supplicant"
 WPA_FILE="wpa_supplicant.conf"
+IDLINE="SVCSENSOR"
+#IDLINE="BGWIFI"
+STATUS_URL="http://52.74.191.39/BluIEQ/getstatus.php"
 
 # Check server status, also used as HTTP keepalive for server. key=val
-curl "http://myhost.com/getstatus.php"| sed 's/\(.*\)=\(.*\)/\1 \2/' | while read key val; do echo $key#$val; done
+curl $STATUS_URL| sed 's/\(.*\)=\(.*\)/\1 \2/' | while read key val; do echo $key#$val; done
 
 function change_wifi_pass() {
     # keep backup of current supplicant file.
@@ -18,7 +21,8 @@ function change_wifi_pass() {
     # https://blog.ergatides.com/2012/01/24/using-sed-to-search-and-replace-contents-of-next-line-in-a-file/
     # http://stackoverflow.com/questions/9063730/how-to-change-a-word-in-a-file-with-linux-shell-script
     # AWK option, http://www.unix.com/unix-for-dummies-questions-and-answers/37430-replace-password-field-using-ed-sed.html
-    sed -i -r "/BGWIFI/I{n; s/.*/\    psk="$1"/}" /etc/wpa_supplicant/wpa_supplicant.conf
+    #sed -i -r "/BGWIFI/I{n; s/.*/\    psk="$1"/}" /etc/wpa_supplicant/wpa_supplicant.conf
+    sed -i -r "/$IDLINE/I{n; s/.*/\    password="$1"/}" /etc/wpa_supplicant/wpa_supplicant.conf
 }
 
 
