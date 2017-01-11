6bc6a6c772b3bf86d049cd3c36fd15cdd74f2324
Jaya <mjay@i2r.a-star.edu.sg>
2017-01-11 15:35:44 +0800

    Added an init file to set permissions, get the refactor file and to set ownerships.

---
+++ tools/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#
+cd dev
+sudo chown -R arkbg.pi *
+wget https://raw.githubusercontent.com/blugraph/blusense/master/tools/refactor.sh
+chmod a+x refactor.sh
+./refactor.sh
+cd devsrc/blusense/tools
+chmod a+x *
