626a79713462a9406ac656c4ba4134deb6dc3748
Jaya <mjay@i2r.a-star.edu.sg>
2017-01-16 16:44:03 +0800

    Modified init script to create a dynamic config file for DEV_ID using command line argument.

--- tools/init.sh
+++ tools/init.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 #
+# Run as eg ./init.sg A1101
 #
 cd dev
 sudo chown -R arkbg.pi *
+
+# Create dev_id config file using command line param.
+if [ -e dev_id.json ]; then
+  echo "Delete existing dev_id.json file!"
+  rm dev_id.json
+fi
+echo "{" >> dev_id.json
+echo "\"DEVICE_ID\":\"$1\"" >> dev_id.json
+echo "}" >> dev_id.json
+#
 wget https://raw.githubusercontent.com/blugraph/blusense/master/tools/refactor.sh
 chmod a+x refactor.sh
 ./refactor.sh
