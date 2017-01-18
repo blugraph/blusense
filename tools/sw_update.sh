ee6fd00dc0d45a8249adb0d1e8aceb3755818b2c
ullas.kumar <ullas.kumar@gmail.com>
2017-01-18 14:14:09 +0800

    34

--- tools/sw_update.sh
+++ tools/sw_update.sh
@@ -63,6 +63,7 @@ function configure_and_make() {
 function wait_and_copy() {
     now=`date +"%m_%d_%Y"`
     #
+
     echo "Backing up current files.."
     mv "$RUN_DIR/$file1" "$BKUP_DIR/$file1-$now"
     mv "$RUN_DIR/$file2" "$BKUP_DIR/$file2-$now"
@@ -70,6 +71,9 @@ function wait_and_copy() {
     mv "$RUN_DIR/$file4" "$BKUP_DIR/$file4-$now"
     mv "$RUN_DIR/$file5" "$BKUP_DIR/$file5-$now"
     mv "$RUN_DIR/$file6" "$BKUP_DIR/$file6-$now"
+    mv "$CF_DIR/$file7" "$BKUP_DIR/$file7-$now"
+    mv "$RUN_DIR/$file8" "$BKUP_DIR/$file8-$now"
+
     sleep 5s
     echo "Copying files to RUN directory.."
     cp "$SW_DIR/apps/sense/$file1" "$RUN_DIR/."
@@ -80,6 +84,7 @@ function wait_and_copy() {
     cp "$SW_DIR/tools/$file5" "$RUN_DIR/."
     cp "$SW_DIR/tools/$file6" "$RUN_DIR/."
     cp "$SW_DIR/apps/config/$file7" "$CF_DIR/."
+    cp "$SW_DIR/apps/sense/$file8" "$RUN_DIR/."
 
     echo "Files copied."
 }
